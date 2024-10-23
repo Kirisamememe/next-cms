51204967f7c7fde6dc4d770c9ef74d8ff262eafe
Kirisame <wuyuhuahen@gmail.com>
2024-10-24 00:01:31 +0900

    add: UIコンポーネントを追加

---
+++ src/components/ui/flexbox.tsx
@@ -0,0 +1,312 @@
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+import React from "react";
+
+
+const flexboxVariants = cva(
+  "min-w-4 min-h-4 ring-offset-background ring-offset-2 focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-2",
+  {
+    variants: {
+      orientation: {
+        vertical: "flex flex-col",
+        horizontal: "flex"
+      },
+      gap: {
+        0.5: "gap-0.5",
+        1: "gap-1",
+        2: "gap-2",
+        3: "gap-3",
+        4: "gap-4",
+        5: "gap-5",
+        6: "gap-6",
+        8: "gap-8",
+        10: "gap-10",
+      },
+      radius: {
+        base: "rounded",
+        md: "rounded-md",
+        lg: "rounded-lg",
+        xl: "rounded-xl",
+        full: "rounded-full",
+      },
+      p: {
+        1: "p-1",
+        2: "p-2",
+        3: "p-3",
+        4: "p-4",
+        5: "p-5",
+        6: "p-6",
+        7: "p-7",
+        8: "p-8",
+      },
+      px: {
+        1: "px-1",
+        2: "px-2",
+        3: "px-3",
+        4: "px-4",
+        5: "px-5",
+        6: "px-6",
+        7: "px-7",
+        8: "px-8",
+      },
+      py: {
+        1: "py-1",
+        2: "py-2",
+        3: "py-3",
+        4: "py-4",
+        5: "py-5",
+        6: "py-6",
+        7: "py-7",
+        8: "py-8",
+      }
+    }
+  }
+)
+
+const Flexbox = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof flexboxVariants> & 
+  { 
+    border?: boolean, 
+    bg?: boolean, 
+    trans?: boolean,
+    centerX?: boolean,
+    centerY?: boolean,
+    center?: boolean
+  }
+>(({ 
+  className, 
+  orientation = "vertical", 
+  radius, 
+  gap, 
+  p, 
+  px, 
+  py, 
+  border = false, 
+  bg = false, 
+  trans = false, 
+  centerX = false,
+  centerY = false,
+  center = false,
+  ...props 
+}, ref) => (
+  <div
+    ref={ref}
+      className={cn(
+        flexboxVariants({ orientation, radius, gap, p, px, py }),
+        border && "border",
+        bg && "bg-card",
+        trans && "transition-all",
+        centerX && orientation === "horizontal" && "justify-center",
+        centerY && orientation === "horizontal" && "items-center",
+        centerX && orientation === "vertical" && "items-center",
+        centerY && orientation === "vertical" && "justify-center",
+        center && "justify-center items-center",
+        className
+      )}
+    {...props}
+  />
+))
+Flexbox.displayName = "Flexbox"
+
+
+const flexRowVariants = cva(
+  "flex min-w-4 min-h-4 ring-offset-background ring-offset-2 focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-2",
+  {
+    variants: {
+      gap: {
+        0.5: "gap-0.5",
+        1: "gap-1",
+        2: "gap-2",
+        3: "gap-3",
+        4: "gap-4",
+        5: "gap-5",
+        6: "gap-6",
+        8: "gap-8",
+        10: "gap-10",
+      },
+      radius: {
+        base: "rounded",
+        md: "rounded-md",
+        lg: "rounded-lg",
+        xl: "rounded-xl",
+        full: "rounded-full",
+      },
+      p: {
+        1: "p-1",
+        2: "p-2",
+        3: "p-3",
+        4: "p-4",
+        5: "p-5",
+        6: "p-6",
+        7: "p-7",
+        8: "p-8",
+      },
+      px: {
+        1: "px-1",
+        2: "px-2",
+        3: "px-3",
+        4: "px-4",
+        5: "px-5",
+        6: "px-6",
+        7: "px-7",
+        8: "px-8",
+      },
+      py: {
+        1: "py-1",
+        2: "py-2",
+        3: "py-3",
+        4: "py-4",
+        5: "py-5",
+        6: "py-6",
+        7: "py-7",
+        8: "py-8",
+      }
+    }
+  }
+)
+
+const FlexRow = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof flexboxVariants> &
+  {
+    border?: boolean,
+    bg?: boolean,
+    trans?: boolean,
+    centerX?: boolean,
+    centerY?: boolean,
+    center?: boolean
+  }
+>(({
+  className,
+  radius,
+  gap,
+  p,
+  px,
+  py,
+  border = false,
+  bg = false,
+  trans = false,
+  centerX = false,
+  centerY = false,
+  center = false,
+  ...props
+}, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      flexRowVariants({ radius, gap, p, px, py }),
+      border && "border",
+      bg && "bg-card",
+      trans && "transition-all",
+      centerX && "justify-center",
+      centerY && "items-center",
+      center && "justify-center items-center",
+      className
+    )}
+    {...props}
+  />
+))
+FlexRow.displayName = "FlexRow"
+
+
+const flexColumnVariants = cva(
+  "flex flex-col min-w-4 min-h-4 ring-offset-background ring-offset-2 focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-2",
+  {
+    variants: {
+      gap: {
+        0.5: "gap-0.5",
+        1: "gap-1",
+        2: "gap-2",
+        3: "gap-3",
+        4: "gap-4",
+        5: "gap-5",
+        6: "gap-6",
+        8: "gap-8",
+        10: "gap-10",
+      },
+      radius: {
+        base: "rounded",
+        md: "rounded-md",
+        lg: "rounded-lg",
+        xl: "rounded-xl",
+        full: "rounded-full",
+      },
+      p: {
+        1: "p-1",
+        2: "p-2",
+        3: "p-3",
+        4: "p-4",
+        5: "p-5",
+        6: "p-6",
+        7: "p-7",
+        8: "p-8",
+      },
+      px: {
+        1: "px-1",
+        2: "px-2",
+        3: "px-3",
+        4: "px-4",
+        5: "px-5",
+        6: "px-6",
+        7: "px-7",
+        8: "px-8",
+      },
+      py: {
+        1: "py-1",
+        2: "py-2",
+        3: "py-3",
+        4: "py-4",
+        5: "py-5",
+        6: "py-6",
+        7: "py-7",
+        8: "py-8",
+      }
+    }
+  }
+)
+
+const FlexColumn = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof flexboxVariants> &
+  {
+    border?: boolean,
+    bg?: boolean,
+    trans?: boolean,
+    centerX?: boolean,
+    centerY?: boolean,
+    center?: boolean
+  }
+>(({
+  className,
+  radius,
+  gap,
+  p,
+  px,
+  py,
+  border = false,
+  bg = false,
+  trans = false,
+  centerX = false,
+  centerY = false,
+  center = false,
+  ...props
+}, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      flexColumnVariants({ radius, gap, p, px, py }),
+      border && "border",
+      bg && "bg-card",
+      trans && "transition-all",
+      centerX && "items-center",
+      centerY && "justify-center",
+      center && "justify-center items-center",
+      className
+    )}
+    {...props}
+  />
+))
+FlexColumn.displayName = "FlexColumn"
+
+export { Flexbox, FlexRow, FlexColumn }
---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,249 @@
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import React from "react";
+
+const headingVariants = cva(
+  "",
+  {
+    variants: {
+      size: {
+        14: "text-sm",
+        16: "text-base",
+        18: "text-lg",
+        20: "text-xl",
+        24: "text-2xl",
+        30: "text-3xl",
+        36: "text-4xl",
+        48: "text-5xl",
+      },
+      height: {
+        1: "leading-[100%]",
+        1.2: "leading-[120%]",
+        1.5: "leading-[150%]",
+      },
+      weight: {
+        400: "font-normal",
+        500: "font-medium",
+        600: "font-semibold",
+        700: "font-bold",
+        800: "font-extrabold",
+        900: "font-black",
+      },
+      color: {
+        foreground: "text-foreground",
+        primary: "text-primary",
+        destructive: "text-destructive",
+        muted: "text-muted-foreground",
+      },
+      py: {
+        0.5: "py-0.5",
+        1: "py-1",
+        2: "py-2",
+      },
+      mb: {
+        0.5: "mb-0.5",
+        1: "mb-1",
+        2: "mb-2",
+        3: "mb-3",
+      }
+    },
+    defaultVariants: {
+      size: 18,
+      color: "foreground",
+      weight: 700
+    }
+  }
+)
+
+const Heading = React.forwardRef<
+  HTMLHeadingElement,
+  React.HTMLAttributes<HTMLHeadingElement> & VariantProps<typeof headingVariants>
+  >(({ className, children, size, height, weight, color, py, mb, ...props }, ref) => (
+  <h6 
+    ref={ref} 
+    className={cn(
+      headingVariants({ size, height, weight, color, py, mb }),
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </h6>
+))
+Heading.displayName = "Heading"
+
+
+
+const paragraphVariants = cva(
+  "",
+  {
+    variants: {
+      size: {
+        12: "text-xs",
+        14: "text-sm",
+        16: "text-base",
+        18: "text-lg",
+        20: "text-xl",
+        24: "text-2xl",
+      },
+      height: {
+        1.2: "leading-[120%]",
+        1.35: "leading-[135%]",
+        1.5: "leading-[150%]",
+        1.65: "leading-[165%]",
+        1.8: "leading-[180%]",
+        2: "leading-[200%]",
+      },
+      weight: {
+        200: "font-extralight",
+        300: "font-light",
+        400: "font-normal",
+        500: "font-medium",
+      },
+      color: {
+        foreground: "text-foreground",
+        muted: "text-muted-foreground",
+        primary: "text-primary",
+        destructive: "text-destructive"
+      },
+      mt: {
+        0.5: "mt-0.5",
+        1: "mt-1",
+        1.5: "mt-1.5",
+        2: "mt-2",
+        3: "mt-3",
+      },
+      mb: {
+        0.5: "mb-0.5",
+        1: "mb-1",
+        1.5: "mb-1.5",
+        2: "mb-2",
+        3: "mb-3",
+      },
+      clamp: {
+        1: "line-clamp-1",
+        2: "line-clamp-2",
+        3: "line-clamp-3",
+        4: "line-clamp-4",
+        5: "line-clamp-5",
+        6: "line-clamp-6",
+      }
+    },
+    defaultVariants: {
+      size: 16,
+      color: "foreground",
+      weight: 400
+    }
+  }
+)
+
+const Paragraph = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement> & VariantProps<typeof paragraphVariants>
+  >(({ className, children, size, height, weight, color, mt, mb, clamp, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn(
+      paragraphVariants({ size, height, weight, color, mt, mb, clamp }),
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </p>
+))
+Paragraph.displayName = "Paragraph"
+
+
+
+
+const labelTextVariants = cva(
+  "",
+  {
+    variants: {
+      size: {
+        10: "text-[0.625rem]",
+        12: "text-xs",
+        14: "text-sm",
+        16: "text-base",
+        18: "text-lg",
+        20: "text-xl",
+        24: "text-2xl",
+        30: "text-3xl",
+        36: "text-4xl",
+        48: "text-5xl",
+      },
+      height: {
+        1: "leading-[100%]",
+        1.1: "leading-[110%]",
+        1.2: "leading-[120%]",
+        1.35: "leading-[135%]",
+        1.5: "leading-[150%]",
+        1.65: "leading-[165%]",
+        1.8: "leading-[180%]",
+        2: "leading-[200%]",
+      },
+      weight: {
+        200: "font-extralight",
+        300: "font-light",
+        400: "font-normal",
+        500: "font-medium",
+        600: "font-semibold",
+        700: "font-bold",
+        800: "font-extrabold",
+        900: "font-black",
+      },
+      color: {
+        foreground: "text-foreground",
+        muted: "text-muted-foreground",
+        primary: "text-primary",
+        destructive: "text-destructive"
+      },
+      mx: {
+        0.5: "mx-0.5",
+        1: "mx-1",
+        1.5: "mx-1.5",
+        2: "mx-2",
+        3: "mx-3",
+      },
+      mt: {
+        0.5: "mt-0.5",
+        1: "mt-1",
+        1.5: "mt-1.5",
+        2: "mt-2",
+        3: "mt-3",
+      },
+      mb: {
+        0.5: "mb-0.5",
+        1: "mb-1",
+        1.5: "mb-1.5",
+        2: "mb-2",
+        3: "mb-3",
+      }
+    },
+    defaultVariants: {
+      size: 12,
+      color: "muted",
+      weight: 400
+    }
+  }
+)
+
+const LabelText = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement> & VariantProps<typeof labelTextVariants>
+  >(({ className, children, size, height, weight, color, mx, mt, mb, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn(
+      labelTextVariants({ size, height, weight, color, mx, mt, mb }),
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </p>
+))
+LabelText.displayName = "LabelText"
+
+export { Heading, Paragraph, LabelText }
