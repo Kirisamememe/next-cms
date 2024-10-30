b8f5f2828cca1b87f41de734b9e974d09ed58883
Kirisame <wuyuhuahen@gmail.com>
2024-10-30 16:07:09 +0900

    update: レイアウトを更新

--- src/app/[locale]/admin/(overview)/editors/layout.tsx
+++ src/app/[locale]/admin/(overview)/editors/layout.tsx
@@ -1,4 +1,3 @@
-import { Flexbox } from "@/components/ui/flexbox";
 import { AuthenticateEditorList } from "./components/authenticate-editors";
 import { UnauthenticatedEditorList } from "./components/unauthenticated-editors";
 import { getSession } from "@/lib/getSession";
@@ -12,11 +11,9 @@ export default async function EditorsPageLayout({
 
   return (
     <>
-      <Flexbox center className="fixed top-0 left-0 w-dvw h-dvh z-[50] pointer-events-none">
-        {children}
-      </Flexbox>
       <AuthenticateEditorList operatorId={operatorId} operatorRole={user.role} />
       <UnauthenticatedEditorList operatorRole={user.role} />
+      {children}
     </>
   )
 }
--- src/app/[locale]/admin/(overview)/editors/loading.tsx
+++ src/app/[locale]/admin/(overview)/editors/loading.tsx
@@ -1,7 +1,10 @@
 import { CircleSpinLoading } from "@/components/circle-spin-loading";
+import { Flexbox } from "@/components/ui/flexbox";
 
 export default function Loading() {
   return (
-    <CircleSpinLoading />
+    <Flexbox center className="fixed top-0 left-0 w-dvw h-dvh z-0 bg-black/50 pointer-events-none">
+      <CircleSpinLoading />
+    </Flexbox>
   )
 }
