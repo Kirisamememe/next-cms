a7b84435a09401de620cbdbddee7846ab23a049b
Kirisame <wuyuhuahen@gmail.com>
2024-10-30 15:42:14 +0900

    update: レイアウトを更新

--- src/app/[locale]/admin/(overview)/editors/[id]/layout.tsx
+++ src/app/[locale]/admin/(overview)/editors/[id]/layout.tsx
@@ -10,9 +10,7 @@ export default async function EditorProfilePageLayout({
 
   return (
     <>
-      <Flexbox center className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-[70%] z-[100]">
-        {children}
-      </Flexbox>
+      {children}
       <Link href={"/admin/editors"}>
         <Flexbox className="faceIn fixed top-0 left-0 w-dvw h-dvh bg-background/80 z-50" />
       </Link>
--- src/app/[locale]/admin/(overview)/editors/layout.tsx
+++ src/app/[locale]/admin/(overview)/editors/layout.tsx
@@ -1,3 +1,4 @@
+import { Flexbox } from "@/components/ui/flexbox";
 import { AuthenticateEditorList } from "./components/authenticate-editors";
 import { UnauthenticatedEditorList } from "./components/unauthenticated-editors";
 import { getSession } from "@/lib/getSession";
@@ -13,7 +14,9 @@ export default async function EditorsPageLayout({
     <>
       <AuthenticateEditorList operatorId={operatorId} operatorRole={user.role} />
       <UnauthenticatedEditorList operatorRole={user.role} />
-      {children}
+      <Flexbox center className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-[70%] z-[100]">
+        {children}
+      </Flexbox>
     </>
   )
 }
