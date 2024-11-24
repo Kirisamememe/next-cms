56f866a8cd46a40ab5fed6bbcac7b614700e2da7
Kirisame <wuyuhuahen@gmail.com>
2024-11-25 00:29:03 +0900

    update: UIを調整

--- src/app/[locale]/admin/_components/header.tsx
+++ src/app/[locale]/admin/_components/header.tsx
@@ -12,14 +12,127 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
 import { Link, usePathname } from "@/i18n/routing";
-import React from "react";
+import React, { ReactNode } from "react";
 import { Separator } from "@/components/ui/separator";
-
 
 export function Header() {
   const pathname = usePathname()
   const t = useTranslations('breadcrumb')
 
+  if (pathname.split('/')[2] === 'gallery') {
+    return (
+      <HeaderContainer>
+        {pathname.split('/').slice(1).map((segment, index, arr) => {
+          if (index === 0) return null
+
+          // リストが一つだけ
+          if (arr.length === 2) {
+            return (
+              <BreadcrumbPage key={segment} className="text-base font-semibold">
+                {t(`${arr[1]}.${index}`)}
+              </BreadcrumbPage>
+            )
+          }
+
+          const href = '/' + arr.slice(0, index + 1).join('/')
+
+          // リストが二つ以上、現在の要素が1番目
+          if (index === 1) {
+            return (
+              <BreadcrumbItem key={segment} className="text-base font-semibold hover:text-foreground">
+                <Link href={href}>
+                  {t(`${arr[1]}.1`)}
+                </Link>
+              </BreadcrumbItem>
+            )
+          }
+
+          if (arr[2] !== 'preview') {
+            if (index < arr.length - 1) {
+              return (
+                <React.Fragment key={segment}>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem className="text-base font-semibold hover:text-foreground">
+                    <Link href={href}>
+                      {decodeURIComponent(segment)}
+                    </Link>
+                  </BreadcrumbItem>
+                </React.Fragment>
+              )
+            }
+
+            return (
+              <React.Fragment key={segment}>
+                <BreadcrumbSeparator />
+                <BreadcrumbPage className="text-base font-semibold">
+                  {decodeURIComponent(segment)}
+                </BreadcrumbPage>
+              </React.Fragment>
+            )
+          }
+
+          if (index > 2) {
+            return null
+          }
+
+          return (
+            <React.Fragment key={segment}>
+              <BreadcrumbSeparator />
+              <BreadcrumbPage className="text-base font-semibold">
+                {t(`${arr[1]}.2`)}
+              </BreadcrumbPage>
+            </React.Fragment>
+          )
+        })}
+      </HeaderContainer>
+    )
+  }
+
+
+  return (
+    <HeaderContainer>
+      {pathname.split('/').slice(1, 4).map((segment, index, arr) => {
+        if (index === 0) return null
+
+        // リストが一つだけ
+        if (arr.length === 2) {
+          return (
+            <BreadcrumbPage key={segment} className="text-base font-semibold">
+              {t(`${arr[1]}.${index}`)}
+            </BreadcrumbPage>
+          )
+        }
+
+        const href = '/' + arr.slice(0, index + 1).join('/')
+
+        // リストが二つ以上、現在の要素が1番目
+        if (index === 1) {
+          return (
+            <BreadcrumbItem key={segment} className="text-base font-semibold hover:text-foreground">
+              <Link href={href}>
+                {t(`${arr[1]}.1`)}
+              </Link>
+            </BreadcrumbItem>
+          )
+        }
+
+        return (
+          <React.Fragment key={segment}>
+            <BreadcrumbSeparator />
+            <BreadcrumbPage className="text-base font-semibold">
+              {t(`${arr[1]}.2${segment.match(/^[0-9]+$/) ? '' : `.${segment}`}`)}
+            </BreadcrumbPage>
+          </React.Fragment>
+        )
+      })}
+    </HeaderContainer>
+  )
+}
+
+
+
+
+function HeaderContainer({ children }: { children: ReactNode }) {
   return (
     <FlexRow className="sticky shrink-0 top-0 h-16 w-full border-b justify-between items-center bg-background/50 backdrop-blur-xl z-50">
       <FlexRow p={3} gap={2} center>
@@ -28,37 +141,7 @@ export function Header() {
         <Heading>
           <Breadcrumb>
             <BreadcrumbList>
-              {pathname.split('/').slice(1, 4).map((segment, index, arr) => {
-                if (index === 0) return null
-                if (arr.length === 2) {
-                  return (
-                    <BreadcrumbPage key={segment} className="text-base font-semibold">
-                      {t(`${arr[1]}.${index}`)}
-                    </BreadcrumbPage>
-                  )
-                }
-
-                const href = '/' + arr.slice(0, index + 1).join('/')
-
-                if (index === 1) {
-                  return (
-                    <BreadcrumbItem key={segment} className="text-base font-semibold hover:text-foreground">
-                      <Link href={href}>
-                        {t(`${arr[1]}.1`)}
-                      </Link>
-                    </BreadcrumbItem>
-                  )
-                }
-
-                return (
-                  <React.Fragment key={segment}>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbPage className="text-base font-semibold">
-                      {t(`${arr[1]}.2${segment.match(/^[0-9]+$/) ? '' : `.${segment}`}`)}
-                    </BreadcrumbPage>
-                  </React.Fragment>
-                )
-              })}
+              {children}
             </BreadcrumbList>
           </Breadcrumb>
         </Heading>
