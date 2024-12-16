a09224ae5b0d5e6a45e25cc3a075805220c75566
Kirisame <wuyuhuahen@gmail.com>
2024-12-16 15:29:02 +0900

    update: UIを改善

--- src/components/ui/rsc-tabs.tsx
+++ src/components/ui/rsc-tabs.tsx
@@ -1,19 +1,27 @@
 "use client"
 
 import * as React from "react"
-import { usePathname } from "next/navigation"
+import { usePathname, useSearchParams } from "next/navigation"
 import { FlexRow } from "@/components/ui/flexbox"
 import { Button } from "@/components/ui/button"
 import { Link } from "@/i18n"
 import { cn } from "@/lib"
+import { useDynamicHeader } from "@/app/[locale]/admin/_components/dynamic-header-provider"
 
 type RootProps = {
   children: React.ReactNode
 }
 
 export function RSCTabs({ children }: RootProps) {
+  const { atTop, isGoingUp } = useDynamicHeader()
+
   return (
-    <FlexRow gap={1} className="bg-muted rounded-lg p-1 shrink-0 w-full @[40rem]:w-fit">
+    <FlexRow
+      gap={1}
+      className={cn(
+        "sticky top-16 bg-card/80 px-8 shrink-0 w-full z-50 backdrop-blur-md border-b gap-6 transition-transform duration-300",
+        (!atTop && !isGoingUp) && "-translate-y-16"
+      )}>
       {children}
     </FlexRow>
   )
@@ -27,17 +35,17 @@ type TabProps = {
 
 export function RSCTabLink({ segment, path, children }: TabProps) {
   const pathname = usePathname()
+  const params = useSearchParams()
 
   return (
     <Button asChild variant={"ghost"}
       className={cn(
-        "w-fit rounded-md h-8 hover:bg-background/40 text-muted-foreground",
+        "w-fit h-11 hover:bg-background/40 text-muted-foreground rounded-none px-0",
         (pathname.endsWith(segment) || (pathname.endsWith(path.split('/')[2]) && segment === 'all')) &&
-        "bg-background hover:bg-background font-semibold shadow-md text-foreground"
+        "font-semibold text-foreground shadow-[inset_0_-2px_0_0_hsl(var(--foreground))]"
       )} >
       <Link
-        href={path}
-        className="w-full"
+        href={`${path}?${params.toString()}`}
       >
         {children}
       </Link>
