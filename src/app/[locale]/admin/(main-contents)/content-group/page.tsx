084916c76c8431c0ea2d3e1433edc511c2ba3d91
Kirisame <wuyuhuahen@gmail.com>
2025-01-16 17:59:44 +0900

    fix: EmptyStateを追加

--- src/app/[locale]/admin/(main-contents)/content-group/page.tsx
+++ src/app/[locale]/admin/(main-contents)/content-group/page.tsx
@@ -3,10 +3,21 @@ import { NewContentBtnContainer } from "../../_components/content/new-content-bu
 import { getTranslations } from "next-intl/server";
 import { GridColumn } from "@/components/ui/grid";
 import { contentGroupService } from "@/di/services";
+import { FlexColumn } from "@/components/ui/flexbox";
+import { NoContentFound } from "@/components/no-article-found";
 
 export default async function Page() {
   const t = await getTranslations()
   const data = await contentGroupService.findMany()
+
+  if (!data.length) {
+    return (
+      <FlexColumn center className="h-full">
+        <NewContentBtnContainer href={'/admin/content-group/new'} label={t('contentGroup.newBtn')} />
+        <NoContentFound text={'no content group'} />
+      </FlexColumn>
+    )
+  }
 
   return (
     <GridColumn p={6} className="appear @[52rem]:grid-cols-2 @[80rem]:grid-cols-3">
